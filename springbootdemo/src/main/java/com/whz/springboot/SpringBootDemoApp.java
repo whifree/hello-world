c7176a98ea40c8cb7c24b83aeec5289946883a4b
whifree <1973680979@qq.com>
2021-10-12 22:14:58 +0800

    4444

--- springbootdemo/src/main/java/com/whz/springboot/SpringBootDemoApp.java
+++ springbootdemo/src/main/java/com/whz/springboot/SpringBootDemoApp.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 public class SpringBootDemoApp {
 
     public static void main(String[] args) {
+		// aaaaa 
         SpringApplication.run(SpringBootDemoApp.class, args);
     }
 }
