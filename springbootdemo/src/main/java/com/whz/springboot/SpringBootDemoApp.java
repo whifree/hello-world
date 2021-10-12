6780975f203494cd32f371432088ac4a8a6c8286
ifree <37031449+whifree@users.noreply.github.com>
2021-10-12 22:16:43 +0800

    Update SpringBootDemoApp.java

--- springbootdemo/src/main/java/com/whz/springboot/SpringBootDemoApp.java
+++ springbootdemo/src/main/java/com/whz/springboot/SpringBootDemoApp.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 public class SpringBootDemoApp {
 
     public static void main(String[] args) {
-		// aaaaa 
+		// aaaaa testtest
         SpringApplication.run(SpringBootDemoApp.class, args);
     }
 }
